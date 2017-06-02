3c03e6809faa5544c958fcf29ea1a91112d96a7f
Chris "Kitt" Burch <christopher.m.burch@gmail.com>
2017-06-02 10:19:05 -0400

    first score alg.

--- borderless.R
+++ borderless.R
@@ -7,38 +7,43 @@ calc_fitness <- function(t_matrix, p_matrix, strat_vec, n_steps = 200, start_pos
   # start_pos = initial current position
   cstep = 1 # step counter
   cpos = start_pos # current position tracker
+  if(v) print(cpos)
   while(cstep <= n_steps){
     # see what's around the current position
     view <- look_around(t_matrix, pos = cpos)
     # check against strategy vector for response to view
-    strat <- find_strat(find_state(p_matrix, view), strat_vec)
+    strat <- as.character(find_strat(find_state(p_matrix, view), strat_vec))
     # initialize a 'score' of 'fitness'
     score <- start_score
     # carry out strategy - move, do nothing, pick up target, etc.
-    # positions: 1 = N, 2 = S, 3 = W, 4 = E, 5 = nothing, 6 = pick up target
+    # positions: 1 = N, 2 = S, 3 = W, 4 = E, 5 = random, 6 = pick up target
     if(v){
-      sprintf("Local view: %d", view)
-      sprintf("Strategy selected: %d", strat)
+      print(sprintf("Local view: %d", view))
+      print(sprintf("Strategy selected: %s", strat))
     }
     
     switch(strat,
-           1 = ifelse(cpos[1] <= 1,
+           "1" = ifelse(cpos[1] <= 1,
                       cpos <- c(ncol(t_matrix), cpos[2]),
                       cpos <- c(cpos[1] - 1, cpos[2])),
-           2 = ifelse(cpos[1] >= nrow(t_matrix),
+           "2" = ifelse(cpos[1] >= nrow(t_matrix),
                       cpos <- c(1, cpos[2]),
                       cpos <- c(cpos[1] + 1, cpos[2])),
-           3 = ifelse(cpos[2] <= 1,
+           "3" = ifelse(cpos[2] <= 1,
                       cpos <- c(cpos[1], ncol(t_matrix)),
                       cpos <- c(cpos[1], cpos[1] - 1)),
-           4 = ifelse(cpos[2] >= ncol(t_matrix),
+           "4" = ifelse(cpos[2] >= ncol(t_matrix),
                       cpos <- c(cpos[1], 1),
                       cpos <- c(cpos[1], cpos[2] + 1)),
-           5 = cpos <- cpos,
-           6 = ifelse(t_matrix[cpos[1], cpos[2]] == 1,
+           "5" = cpos <- c(cpos[1] + sample(c(-1, 0, 1), 1),
+                           cpos[2] + sample(c(-1, 0, 1), 1)),
+           "6" = ifelse(t_matrix[cpos[1], cpos[2]] == 1,
                       score <- score + 5,
                       score <- score - 3)
           )
+    if(v){
+      sprintf("current fitness: %d", score)
+    }
     # iterate step counter
     cstep <- cstep + 1
   }
--- common_cas.R
+++ common_cas.R
@@ -38,11 +38,11 @@ look_around <- function(t_matrix, pos = c(1, 1)){
                     t_matrix[pos[1] + 1, pos[2]])
   # check W
   view[3] <- ifelse(pos[2] <= 1,
-                    t_matrix[pos[1], 1],
+                    t_matrix[pos[1], ncol(t_matrix)],
                     t_matrix[pos[1], pos[2] - 1])
   # check E
   view[4] <- ifelse(pos[2] >= ncol(t_matrix),
-                    t_matrix[pos[1], ncol(t_matrix)],
+                    t_matrix[pos[1], 1],
                     t_matrix[pos[1], pos[2] + 1])
   # check underfoot
   view[5] <- t_matrix[pos[1], pos[2]]
